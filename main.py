aca471b4a8f5c14b60832a212b027ceaeb99f1ce
Alex Kouris <alex.kouris@saphetor.com>
2023-02-09 01:26:44 +0200

    Annotate with Gene Ontology

--- main.py
+++ main.py
@@ -57,6 +57,42 @@ def number_of_entries(connection, table_name):
     return result.fetchone()[0]
 
 
+def unique_proteins_of_table(connection, table_name, print_unique_info):
+    unique_accessions = []
+    res = connection.execute(f"SELECT DISTINCT accession FROM {table_name}")
+
+    for entry in res.fetchall():
+        accession_id = entry[0]
+        unique_accessions.append(accession_id)
+        if print_unique_info:
+            res2 = connection.execute(
+                f"SELECT description, brain_part, coverage FROM {table_name} WHERE accession='{accession_id}'")
+            for protein in res2.fetchall():
+                print(f"Found unique protein {accession_id} ({protein[0]}) in {protein[1]} with coverage={protein[2]}")
+
+    print(f"We have {len(unique_accessions)} unique accessions")
+    return unique_accessions
+
+
+def annotate_dataframes(dataframes_to_annotate, gene_ontology_df, dataframes_accession_id, gene_ontology_accession_id):
+    """
+    Annotate dataframes with extra columns found in gene_ontology based on the column name for matching between dataframes
+    :param dataframes_to_annotate: list of dataframes to annotate
+    :param gene_ontology_df: gene ontology dataframe
+    :param dataframes_accession_id: column name in dataframes that will try to match in gene_ontology dataframe
+    :param gene_ontology_accession_id: column name in gene ontology that contains the accession id
+    :return: list of annotated dataframes
+    """
+    for df in dataframes_to_annotate:
+        for _, row in gene_ontology_df.iterrows():
+            updated = df[dataframes_accession_id] == row[gene_ontology_accession_id]
+            df.loc[updated, 'gene_names'] = row['Gene Names']
+            df.loc[updated, 'biological_process'] = row['Gene Ontology (biological process)']
+            df.loc[updated, 'gene_ontology'] = row['Gene Ontology (GO)']
+            df.loc[updated, 'molecular_function'] = row['Gene Ontology (molecular function)']
+            df.loc[updated, 'cellular_component'] = row['Gene Ontology (cellular component)']
+
+
 def open_files_from_directory(directory):
     """
     Opens any excel files in the directory and returns DataFrames for each file
@@ -86,6 +122,13 @@ for brain_df in hrms:
 
 db_con = sqlite3.connect("biotechnology.db")
 
+gene_ontology = pd.read_excel("uniprot-gene-ontology.xlsx")
+
+annotate_dataframes(dataframes_to_annotate=hrms,
+                    gene_ontology_df=gene_ontology,
+                    dataframes_accession_id="accession",
+                    gene_ontology_accession_id="Entry")
+
 if not table_exists(connection=db_con, table_name="hrms"):
     for brain_df in hrms:
         brain_df.to_sql(name='hrms', if_exists='append', con=db_con, index=False)
@@ -93,16 +136,6 @@ if not table_exists(connection=db_con, table_name="hrms"):
 
 print(f"Table HRMS has {number_of_entries(connection=db_con, table_name='hrms')} entries")
 
-unique_accessions = []
-res = db_con.execute("SELECT DISTINCT accession FROM hrms")
-
-for entry in res.fetchall():
-    accession_id = entry[0]
-    unique_accessions.append(accession_id)
-    res2 = db_con.execute(f"SELECT description, brain_part, coverage FROM hrms WHERE accession='{accession_id}'")
-    for protein in res2.fetchall():
-        print(f"Found unique protein {accession_id} ({protein[0]}) in {protein[1]} with coverage={protein[2]}")
-
-print(f"We have {len(unique_accessions)} unique accessions")
+unique_proteins_of_table(db_con, "hrms", False)
 
 db_con.close()
