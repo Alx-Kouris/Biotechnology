6695dbc62190b17796335c466fe8d054756550a5
Alex Kouris <alex.kouris@saphetor.com>
2023-02-14 10:13:30 +0200

    Import Jung

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def unique_proteins_of_table(connection, table_name, part="", print_unique_info=
     return unique_accessions
 
 
-def annotate_dataframes(dataframes_to_annotate, gene_ontology_df, dataframes_accession_id, gene_ontology_accession_id):
+def annotate_dataframes(dataframes_to_annotate, gene_ontology_df, dataframes_accession_id, gene_ontology_accession_id, add_protein_identifier = False):
     """
     Annotate dataframes with extra columns found in gene_ontology based on the column name for matching between dataframes
     :param dataframes_to_annotate: list of dataframes to annotate
@@ -100,12 +100,14 @@ def annotate_dataframes(dataframes_to_annotate, gene_ontology_df, dataframes_acc
     """
     for df in dataframes_to_annotate:
         for _, row in gene_ontology_df.iterrows():
-            updated = df[dataframes_accession_id] == row[gene_ontology_accession_id]
+            updated = df[dataframes_accession_id] == str(row[gene_ontology_accession_id]).split(" ")[0]
             df.loc[updated, 'gene_names'] = row['Gene Names']
             df.loc[updated, 'biological_process'] = row['Gene Ontology (biological process)']
             df.loc[updated, 'gene_ontology'] = row['Gene Ontology (GO)']
             df.loc[updated, 'molecular_function'] = row['Gene Ontology (molecular function)']
             df.loc[updated, 'cellular_component'] = row['Gene Ontology (cellular component)']
+            if add_protein_identifier:
+                df.loc[updated, 'protein_identifier'] = row['Entry Name']
 
 
 def cleanup_column_names(dataframe):
@@ -140,7 +142,6 @@ def open_files_from_directory(directory):
 ###################################################################
 
 gene_ontology = pd.read_excel("uniprot-gene-ontology.xlsx")
-
 
 # Open SQL connection
 db_con = sqlite3.connect("biotechnology.db")
@@ -221,7 +222,6 @@ if not table_exists(connection=db_con, table_name=taraslia_table_name):
 
     taraslia.to_sql(name=taraslia_table_name, if_exists='append', con=db_con, index=False)
 
-
 print(f"Table {taraslia_table_name} has {number_of_entries(connection=db_con, table_name=taraslia_table_name)} entries")
 
 unique_proteins_of_table(connection=db_con, table_name=taraslia_table_name)
@@ -240,5 +240,57 @@ for study in studies:
         print(f"{brain_part.upper()} in study {study.upper()} has "
               f"{len(unique_proteins_of_table(connection=db_con, table_name=study, part=brain_part))} unique proteins.")
 
+#####################################################################################################################
+
+jung_match_parts = {
+    "Olfactory": "olfactory_balb",
+    "STR": "striatum",
+    "CTX1": "cortex",
+    "CTX2": "cortex",
+    "TH1": "thalamus",
+    "TH2": "thalamus",
+    "PA": "cortex",
+    "HY1": "hipothalamus",
+    "HY2": "hipothalamus",
+    "CXS": "cortex",
+    "HP": "hipocampus",
+    "MB": "mid_brain",
+    "PO": "pons",
+    "RHP": "hipocampus",
+    "CB1": "cerebellum",
+    "CB2": "cerebellum",
+    "MY": "medulla"
+}
+
+jung_table_name = "jung"
+
+if not table_exists(connection=db_con, table_name=jung_table_name):
+    jung_df = pd.DataFrame()
+    for key in jung_match_parts.keys():
+        print(f"Reading sheet {key} from Jung")
+        jung = pd.read_excel("/home/kourisa/Downloads/Project/Mouse brain 2,3/Jung et al 2017/mcp.M116.061440-7.xlsx",
+                             key, skiprows=[0, 1])
+        jung = jung.drop_duplicates(subset='Gene ID')  # keep a gene only once from each sample
+        jung = jung.drop(columns=['Sample', 'Recovered Sequence ( under score delimited)'])
+        jung = jung.reset_index()
+
+        jung['brain_part'] = jung_match_parts[key]
+        jung_df = pd.concat([jung_df, jung])
+
+    jung_df.reset_index()
+    jung_df.drop(columns=['index'])
+    jung_df = jung_df.drop_duplicates(subset=['Gene ID', 'brain_part'])  # keep a gene only once from each sample
+    print(jung_df['brain_part'].value_counts())
+    cleanup_column_names(jung_df)
+    jung_df.rename(columns={"full_name": "description"}, inplace=True)
+
+    # Annotate with gene ontology before storing in database
+    annotate_dataframes(dataframes_to_annotate=[jung_df],
+                        gene_ontology_df=gene_ontology,
+                        dataframes_accession_id="symbol",
+                        gene_ontology_accession_id="Gene Names",
+                        add_protein_identifier=True)
+
+    jung_df.to_sql(name=jung_table_name, if_exists='append', con=db_con, index=False)
 
 db_con.close()
